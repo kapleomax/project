ddf2aa0a15b9d0d4e2d0b0b797996e316d717c40
Dan <example@gmail.com>
2023-09-12 16:33:29 +0300

    practice part1

--- js/script.js
+++ js/script.js
@@ -1,19 +1,2 @@
 "use strict";
 
-console.log('arr ' + "- object");
-
-console.log(4 + +"5")
-
-let incr = 10,
-    decr = 10;
-
-incr++;
-decr--;
-
-console.log(incr);
-console.log(decr);
-
-const isChecked = true,
-      isClose = false
-
-console.log(isChecked || isClose);п
